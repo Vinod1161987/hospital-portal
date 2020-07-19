7f297b8ddf8f1c8d58de3f12e8017567c7ebadaa
Sandeep <Sandeep.Satpute53@gmail.com>
2020-07-19 16:57:01 +0530

    Changed token storage from local to sesssion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,41 +10,36 @@ import Constants from '../helpers/constants';
   providedIn: 'root'
 })
 export class AuthService {
-private loggedInUserSubject: BehaviorSubject<LoginResponse>;
-private loggedInUser:  Observable<LoginResponse>;
+  private loggedInUserSubject: BehaviorSubject<LoginResponse>;
+  private loggedInUser: Observable<LoginResponse>;
   constructor(private httpClientService: HttpClientService) {
     this.loggedInUserSubject = new BehaviorSubject<LoginResponse>(JSON.parse(localStorage.getItem("user")));
     this.loggedInUser = this.loggedInUserSubject.asObservable();
-   }
+  }
 
-   public getLoggedInUser(): LoginResponse {
-      return this.loggedInUserSubject.value;
-   }
+  public getLoggedInUser(): LoginResponse {
+    return this.loggedInUserSubject.value;
+  }
 
-   login(username:string, password: string){
-     const headers = [];
-     headers.push({key:Constants.userId,value:username})
-     headers.push({key:Constants.userPassword,value:password})
-     return this.httpClientService.post("user/authenticate",{},headers)
-     .pipe(
-       map(res=>
-        {
-          if(res.statusCode===200)
-          {
-            localStorage.setItem(Constants.accessToken_lsKey,JSON.stringify(res.data.accessToken));
-            localStorage.setItem(Constants.loggedInUser_lsKey,JSON.stringify(res.data));
+  login(username: string, password: string) {
+    const headers = [];
+    headers.push({ key: Constants.userId, value: username })
+    headers.push({ key: Constants.userPassword, value: password })
+    return this.httpClientService.post("user/authenticate", {}, headers)
+      .pipe(
+        map(res => {
+          if (res.statusCode === 200) {
+            sessionStorage.setItem(Constants.accessToken_lsKey, JSON.stringify(res.data.accessToken));
+            sessionStorage.setItem(Constants.loggedInUser_lsKey, JSON.stringify(res.data));
             this.loggedInUserSubject.next(res.data)
           }
           return res;
-       })
-     );
-     // call http post method - to authorize user
-     // response - set local storage item
-     // set BehaviourSubject = reponse object
-   }
-   logout(){
-     localStorage.removeItem(Constants.accessToken_lsKey);
-     localStorage.removeItem(Constants.loggedInUser_lsKey);
-     this.loggedInUserSubject.next(null);
-   }
+        })
+      );
+  }
+  logout() {
+    sessionStorage.removeItem(Constants.accessToken_lsKey);
+    sessionStorage.removeItem(Constants.loggedInUser_lsKey);
+    this.loggedInUserSubject.next(null);
+  }
 }
